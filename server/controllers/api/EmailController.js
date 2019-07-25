af31bbaa30326e4bf6faa551559dee12fd2deafc
Arya Stark Dev <chyke.ejike232@gmail.com>
2019-07-25 14:12:43 -0400

    change email text to html format

--- server/controllers/api/EmailController.js
+++ server/controllers/api/EmailController.js
@@ -8,12 +8,13 @@ const sendEmail = async (req, res) => {
     to: `${req.body.receiverName} <${req.body.receiver}>`,
     from: `${req.body.senderName} <${req.body.sender}>`,
     subject: req.body.subject,
-    text: req.body.content,
+    html: req.body.content,
   };
 
   try {
-    const response = await sendGridMailService.send(msg);
-    return res.status(200).json({ data: response });
+    // const response = await sendGridMailService.send(msg);
+    console.log({msg})
+    return res.status(200).json({ data: 'I was sent' });
   } catch (error) {
     return res.status(500).json({ data: error });
   }
